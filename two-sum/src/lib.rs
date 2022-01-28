8610afa36f1cd7c16b54d99180d812c5f641a723
fitv <rustids@gmail.com>
2022-01-28 20:05:51 +0800

    two sum

--- two-sum/src/lib.rs
+++ two-sum/src/lib.rs
@@ -8,37 +8,23 @@ mod tests {
     fn test_two_sum() {
         let nums = [2, 7, 11, 15];
 
-        assert_eq!(Some((0, 1)), two_sum(&nums, 9));
-        assert_eq!(Some((1, 2)), two_sum(&nums, 18));
-        assert_eq!(Some((2, 3)), two_sum(&nums, 26));
+        assert!(Some((0, 1)) == two_sum(&nums, 9) || Some((1, 0)) == two_sum(&nums, 9));
+        assert!(Some((1, 2)) == two_sum(&nums, 18) || Some((2, 1)) == two_sum(&nums, 18));
+        assert!(Some((2, 3)) == two_sum(&nums, 26) || Some((3, 2)) == two_sum(&nums, 26));
         assert_eq!(None, two_sum(&nums, 100));
     }
 }
 
 pub fn two_sum(nums: &[i32], target: i32) -> Option<(usize, usize)> {
-    let map = {
-        let mut map = HashMap::new();
+    let mut map = HashMap::new();
 
-        for (i, &val) in nums.iter().enumerate() {
-            map.insert(val, i);
-        }
-        map
-    };
-
-    for (val, i) in &map {
-        let elem = map.get(&(target - val));
+    for (i, _) in nums.iter().enumerate() {
+        let elem = map.get(&nums[i]);
 
         if elem.is_some() {
-            return Some({
-                let (x, y) = (*i, *elem.unwrap());
-
-                if x > y {
-                    (y, x)
-                } else {
-                    (x, y)
-                }
-            });
+            return Some((i, *elem.unwrap()));
         }
+        map.insert(target - nums[i], i);
     }
     None
 }
