315dfb6bdc7e69af444cd34f723c9b9e417eebc2
fitv <rustids@gmail.com>
2022-01-29 17:55:23 +0800

    Longest Substring Without Repeating Characters

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 - [1. Two Sum](./two-sum/src/lib.rs)
 - [2. Add Two Numbers](./add-two-numbers/src/lib.rs)
+- [3. Longest Substring Without Repeating Characters](./longest-substring-without-repeating-characters/src/lib.rs)
---
+++ longest-substring-without-repeating-characters/src/lib.rs
@@ -0,0 +1,42 @@
+use std::cmp;
+use std::collections::HashMap;
+
+#[cfg(test)]
+mod tests {
+    use crate::Solution;
+
+    #[test]
+    fn test_length_of_longest_substring() {
+        let s = String::from("pwwkew");
+        assert_eq!(Solution::length_of_longest_substring(s), 3);
+
+        let s = String::from("aaaa");
+        assert_eq!(Solution::length_of_longest_substring(s), 1);
+
+        let s = String::from("dvdf");
+        assert_eq!(Solution::length_of_longest_substring(s), 3);
+    }
+}
+
+pub struct Solution {}
+
+impl Solution {
+    pub fn length_of_longest_substring(s: String) -> i32 {
+        let mut len = 0;
+        let chars: Vec<_> = s.chars().collect();
+        let mut map = HashMap::new();
+        let mut cursor = 0;
+
+        for i in 0..s.len() {
+            let idx = map.get(&chars[i]);
+
+            if idx.is_some() {
+                cursor = cmp::max(cursor, *idx.unwrap() + 1);
+            }
+            len = cmp::max(len, i - cursor + 1);
+
+            map.insert(chars[i], i);
+        }
+        return len as i32;
+    }
+}
