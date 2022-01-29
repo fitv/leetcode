c448557cfbb0182443a4ec9091349cb72d4ad4f3
fitv <rustids@gmail.com>
2022-01-29 17:56:35 +0800

    Longest Substring Without Repeating Characters

--- longest-substring-without-repeating-characters/src/lib.rs
+++ longest-substring-without-repeating-characters/src/lib.rs
@@ -22,10 +22,10 @@ pub struct Solution {}
 
 impl Solution {
     pub fn length_of_longest_substring(s: String) -> i32 {
-        let mut len = 0;
-        let chars: Vec<_> = s.chars().collect();
-        let mut map = HashMap::new();
+        let mut max_len = 0;
         let mut cursor = 0;
+        let mut map = HashMap::new();
+        let chars: Vec<_> = s.chars().collect();
 
         for i in 0..s.len() {
             let idx = map.get(&chars[i]);
@@ -33,10 +33,10 @@ impl Solution {
             if idx.is_some() {
                 cursor = cmp::max(cursor, *idx.unwrap() + 1);
             }
-            len = cmp::max(len, i - cursor + 1);
+            max_len = cmp::max(max_len, i - cursor + 1);
 
             map.insert(chars[i], i);
         }
-        return len as i32;
+        return max_len as i32;
     }
 }
