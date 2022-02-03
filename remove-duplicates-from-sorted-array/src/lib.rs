ae5ae6da4778f76f7fe64bb867e53ae2f7e74c87
fitv <rustids@gmail.com>
2022-02-03 18:06:52 +0800

    Remove Duplicates from Sorted Array

--- remove-duplicates-from-sorted-array/src/lib.rs
+++ remove-duplicates-from-sorted-array/src/lib.rs
@@ -6,11 +6,11 @@ mod tests {
     fn test_remove_duplicates() {
         let mut nums = vec![1, 1, 2];
         assert_eq!(Solution::remove_duplicates(&mut nums), 2);
-        assert_eq!(nums, vec![1, 2]);
+        assert_eq!(nums.get(0..2).unwrap(), vec![1, 2]);
 
         let mut nums = vec![0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
         assert_eq!(Solution::remove_duplicates(&mut nums), 5);
-        assert_eq!(nums, vec![0, 1, 2, 3, 4]);
+        assert_eq!(nums.get(0..5).unwrap(), vec![0, 1, 2, 3, 4]);
     }
 }
 
@@ -30,10 +30,6 @@ impl Solution {
                 len += 1;
             }
         }
-        unsafe {
-            nums.set_len(len);
-        }
-
         len as i32
     }
 }
