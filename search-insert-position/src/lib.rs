36c11d58c72f2696452c85d97821ad992ae94459
fitv <rustids@gmail.com>
2022-02-04 16:46:51 +0800

    Search Insert Position

---
+++ search-insert-position/src/lib.rs
@@ -0,0 +1,26 @@
+#[cfg(test)]
+mod tests {
+    use crate::Solution;
+
+    #[test]
+    fn test_search_insert() {
+        assert_eq!(Solution::search_insert(vec![1, 3, 5, 6], 5), 2);
+        assert_eq!(Solution::search_insert(vec![1, 3, 5, 6], 2), 1);
+        assert_eq!(Solution::search_insert(vec![1, 3, 5, 6], 7), 4);
+        assert_eq!(Solution::search_insert(vec![1, 3, 5, 6], 0), 0);
+        assert_eq!(Solution::search_insert(vec![1], 0), 0);
+    }
+}
+
+pub struct Solution {}
+
+impl Solution {
+    pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
+        for i in 0..nums.len() {
+            if nums[i] >= target {
+                return i as i32;
+            }
+        }
+        nums.len() as i32
+    }
+}
